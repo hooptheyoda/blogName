e665a30af8ca225cf62449d735d1051f184d0cdc
John Wallace <Wallacej2@student.lasalle.edu>
2017-11-10 16:57:43 -0500

    add js
    added js for carousel

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,16 @@
 console.log('Hello world');
+
+var myIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("SS");
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}
+    x[myIndex-1].style.display = "block";
+    setTimeout(carousel, 2000);
+}
